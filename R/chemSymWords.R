96e4223e3189ee9c66c11f31289d95533aa02287
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-03-03 22:08:27 -0500

    update main function

--- R/chemSymWords.R
+++ R/chemSymWords.R
@@ -2,21 +2,32 @@
 ## generate chem words
 ##
 
-elements = c("h","he","li","be","b","c","n","o","f","ne","na","mg","al","si","p","s","cl","ar","k","ca","sc","ti","v","cr","mn","fe","co","ni","cu","zn","ga","ge","as","se","br","kr","rb","sr","y","zr","nb","mo","tc","ru","rh","pd","ag","cd","in","sn","sb","te","i","xe","cs","ba","la","ce","pr","nd","pm","sm","eu","gd","tb","dy","ho","er","tm","yb","lu","hf","ta","w","re","os","ir","pt","au","hg","tl","pb","bi","po","at","rn","fr","ra","ac","th","pa","u","np","pu","am","cm","bk","cf","es","fm","md","no","lr","rf","db","sg","bh","hs","mt","ds","rg","cn","uut","fl","uup","lv","uus","uuo")
-
+#' chemWord
+#'
+#' Generate word using chemical elements
+#'
+#' @export
 chemWord <- function(w, sym=elements) {
-  return (chemWordRecurse(w, list(), sym))
+  symbolList <- chemWordRecurse(w, list(), sym$Symbol)
+  if (length(symbolList) == 0) {
+    return(FALSE)
+  } else {
+    ref <- which.min(lapply(symbolList, length))
+    return(sym$Symbol[symbolList[[ref]]])
+  }
 }
 
-chemWordRecurse <- function(w, t, sym){
+chemWordRecurse <- function(w, t, sym=elements) {
   out <- list()
+
   chemWordRecurseInternal <- function(w, t, sym) {
     if (nchar(w) == 0) {
       out[[length(out) + 1]] <<- unlist(t)
     }
     for (i in 1:min(2, nchar(w))) {
-      if (substring(w, 1, i) %in% sym) {
-        Recall(substring(w, i + 1), c(t, substring(w, 1, i)), sym)
+      find_el <- match(toupper(substring(w, 1, i)),toupper(sym))
+      if (!is.na(find_el)) {
+        Recall(substring(w, i + 1), c(t, find_el), sym)
       }
     }
   }
