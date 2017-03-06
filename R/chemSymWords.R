f862940262db341be9c5e578cff03f40db6af492
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-03-06 16:11:32 -0500

    chemWordPNG

--- R/chemSymWords.R
+++ R/chemSymWords.R
@@ -28,6 +28,7 @@ chemWord <- function(w, output='min', sym=elements) {
   }
 }
 
+
 #' recurse a string identifying chemical elements
 #'
 #' Recursive function to find chemical elements within a string
@@ -57,3 +58,62 @@ chemWordRecurse <- function(w, t, sym=elements) {
   return(out)
 }
 
+
+#' Create a PNG representation using chemical elements
+#'
+#' Output a PNG file of chemical elements for a particular word
+#'
+#' @param w Character.  A word to convert to chemical elements
+#' @param sym Data Frame.  A data frame containing chemical information.  Defaults to elements
+#' @param f Character.  Filename for output file (including path)
+#'
+#' @return Boolean.  True if file generated, false if word could not be represented by chemical elements
+#'
+#' @import grid
+#'
+#' @export
+chemWordPNG <- function(w, sym=elements, f='chemWord.png') {
+  symbolList <- chemWordRecurse(w, list(), sym$Symbol)
+  if (length(symbolList) == 0) {
+    return(FALSE)
+  } else {
+    chemRef <- symbolList[[which.min(lapply(symbolList, length))]]
+    nboxes <- length(chemRef)  ## Number of elements
+    boxSize <- 100  ## Size for each symbol (in pixels)
+    totalWidth <- nboxes * 1.06 * boxSize  ## Width of png output (in pixels)
+    png(f, width = totalWidth, height = boxSize * 1.06, res = 72)
+    vp = viewport(x = 0.5, y = 0.5, width = unit(totalWidth, 'points'), height = unit(boxSize, 'points'))
+    grid.newpage()
+    pushViewport(vp)
+
+    for (i in 1:nboxes) {
+      xC <- boxSize / 2 + (i-1)*boxSize + ((i-1)*2+1)*0.03*boxSize
+      yC <- boxSize / 2
+      grid.rect(x = xC, y = yC, width = boxSize, height = boxSize, default.units = "points")
+      grid.text(label = sym$Symbol[chemRef[i]],
+                x = xC,
+                y = yC,
+                just = c('center', 'center'),
+                default.units = "points",
+                gp=gpar(col='black', fontsize = 0.33 * boxSize))  ## Chemical Symbol
+      xTop <- xC - boxSize * 0.45
+      yTop <- boxSize * 0.9
+      grid.text(label = sym$Atomic_Number[chemRef[i]],
+                x = xTop,
+                y = yTop,
+                just = c('left', 'center'),
+                default.units = 'points',
+                gp = gpar(col = 'black', fontsize = 0.15 * boxSize))  ## top line - atomic number
+      xBottom <- xC
+      yBottom <- boxSize * 0.15
+      grid.text(label = sym$Name[chemRef[i]],
+                x = xBottom,
+                y = yBottom,
+                just = c('center', 'center'),
+                default.units = 'points',
+                gp = gpar(col = 'black', fontsize = 0.15 * boxSize))  ## bottom line - name
+    }
+    dev.off()
+    return(TRUE)
+  }
+}
