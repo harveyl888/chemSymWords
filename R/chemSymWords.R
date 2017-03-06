54796d158cdeef8b7fd5c49031e5e38c61abc24c
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-03-06 09:16:13 -0500

    document

--- R/chemSymWords.R
+++ R/chemSymWords.R
@@ -1,10 +1,11 @@
-##
-## generate chem words
-##
-
-#' chemWord
+#' Represent a string using chemical elements
 #'
-#' Generate word using chemical elements
+#' Represent a string using chemical elements
+#'
+#' @param w Character.  A word to convert to chemical elements
+#' @param sym Data Frame.  A data frame containing chemical information.  Defaults to elements
+#'
+#' @return Character vector of elements if successful or FALSE if not all characters can be assinged to elements
 #'
 #' @export
 chemWord <- function(w, sym=elements) {
@@ -17,6 +18,17 @@ chemWord <- function(w, sym=elements) {
   }
 }
 
+#' recurse a string identifying chemical elements
+#'
+#' Recursive function to find chemical elements within a string
+#'
+#' @param w Character.  Text to search for chemical elements
+#' @param t List.  Running list of partial matches for current search path
+#' @param sym Data frame.  A data frame containing chemical information
+#'
+#' @return List of results.  Each list item refers to a positive result in representing the
+#'   input string as a series of elements.  List items are vectors corresponding to row
+#'   positions of the elements in \code{sym}.
 chemWordRecurse <- function(w, t, sym=elements) {
   out <- list()
 
