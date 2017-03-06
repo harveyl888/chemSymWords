2f6e75bc6fefa019c4f6024852ab7e458544decc
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-03-06 09:45:52 -0500

    allow symbols > 2 chars

--- R/chemSymWords.R
+++ R/chemSymWords.R
@@ -46,7 +46,7 @@ chemWordRecurse <- function(w, t, sym=elements) {
     if (nchar(w) == 0) {
       out[[length(out) + 1]] <<- unlist(t)
     }
-    for (i in 1:min(2, nchar(w))) {
+    for (i in 1:min(max(nchar(sym)), nchar(w))) {
       find_el <- match(toupper(substring(w, 1, i)),toupper(sym))
       if (!is.na(find_el)) {
         Recall(substring(w, i + 1), c(t, find_el), sym)
