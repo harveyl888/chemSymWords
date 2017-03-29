4bb4cb95ad4135cec563e3e3285ce274d441bd11
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-03-28 22:59:32 -0400

    documentation

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Chemical Elements
+#'
+#' A dataframe containing details for the first 118 elements
+#'
+#' @format A data frame with 118 rows and 4 variables:
+#' \describe{
+#'   \item{Atomic_Number}{atomic number}
+#'   \item{Symbol}{chemical symbol}
+#'   \item{Name}{chemical name}
+#'   \item{Atomic_Mass}{atomic mass}
+#' }
+"elements"
